50f157f7b3258416c9940ba13fd22798cc5b86ff
Gerbrand van Dieijen <gerbrand@vandieijen.nl>
2023-05-13 01:43:03 +0200

    Start project with kafka in docker-compose. Based on https://github.com/zio/zio-kafka

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,53 @@
+import zio._
+import zio.Console._
+import zio._
+import zio.kafka.consumer._
+import zio.kafka.producer.{Producer, ProducerSettings}
+import zio.kafka.serde._
+import zio.stream.ZStream
+
+object MyApp extends ZIOAppDefault {
+
+  val producer: ZStream[Producer, Throwable, Nothing] =
+    ZStream
+      .repeatZIO(Random.nextIntBetween(0, Int.MaxValue))
+      .schedule(Schedule.fixed(2.seconds))
+      .mapZIO { random =>
+        Producer.produce[Any, Long, String](
+          topic = "random",
+          key = random % 4,
+          value = random.toString,
+          keySerializer = Serde.long,
+          valueSerializer = Serde.string
+        )
+      }
+      .drain
+
+  val consumer: ZStream[Consumer, Throwable, Nothing] =
+    Consumer
+      .plainStream(Subscription.topics("random"), Serde.long, Serde.string)
+      .tap(r => Console.printLine(r.value))
+      .map(_.offset)
+      .aggregateAsync(Consumer.offsetBatches)
+      .mapZIO(_.commit)
+      .drain
+
+  def producerLayer =
+    ZLayer.scoped(
+      Producer.make(
+        settings = ProducerSettings(List("localhost:9092"))
+      )
+    )
+
+  def consumerLayer =
+    ZLayer.scoped(
+      Consumer.make(
+        ConsumerSettings(List("localhost:9092")).withGroupId("group")
+      )
+    )
+
+  override def run =
+    producer.merge(consumer)
+      .runDrain
+      .provide(producerLayer, consumerLayer)
+}
